c317264445bc20e1924bf396f18f7513ac60daf9
Tiffany <tchan12899@gmail.com>
2024-02-05 22:06:02 -0500

    Added user seeds

---
+++ seeds/User.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        name: 'Xandromus',
+        email: 'xandromus123@hotmail.com',
+        password: 'password123'
+    },
+    {
+        name: 'Lernantino',
+        email: 'lernantino234@yahoo.com',
+        password: 'password234'
+    }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
