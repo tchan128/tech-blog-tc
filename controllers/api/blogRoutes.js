663936d1d8200f83deacc9b8fe45d11d89ea1d2f
Tiffany <tchan12899@gmail.com>
2024-03-09 16:43:25 -0500

    Added blogRoutes and set up POST to create blog

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBlog)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+module.exports = router;
